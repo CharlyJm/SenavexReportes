0e28785d12e3e887241d551691ce28a35025c24d
unknown <elianacosme>
2022-11-23 17:31:19 -0400

    home los botones  del controlador

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,61 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-        <user-component> </user-component>
-        <listado-component> </listado-component>
+        <div class="col-md-2">
+                       <form action="{{url('paises/export')}}" enctype="multipart/form-data">
+                           <button class="btn btn-success">Exportar Paises</button>
+                           
+                       </form>
+                    </div>
+               
+                    <div class="col-md-2">
+                       <form action="{{url('empresass/export')}}" enctype="multipart/form-data">
+                           <button class="btn btn-success">Exportar Empresas</button>
+                       </form>
+                    </div>
+<!-- 
+                    ///// -->
+                    
+                    <h1> DDJJ   PDF</h1>
+                    <div class="col-md-2">
+                       <form action="{{url('PDF')}}" enctype="multipart/form-data">
+                           <button class="btn btn-success">pdf</button>
+                       </form>
+                       
+                    </div>
+                    
+                    <div class="col-md-2">
+                       <form action="{{url('index')}}" enctype="multipart/form-data">
+                           <button class="btn btn-success">index</button>
+                       </form>
+                    </div>
+                    <div class="col-md-2">
+                       <form action="{{url('export')}}" enctype="multipart/form-data">
+                           <button class="btn btn-success">Exportar ddjj</button>
+                       </form>
+                    </div>
+                       
+                    <h1> RRCO REPORTES</h1>
+                    <div class="col-md-2">
+                       <form action="{{url('index/RRCO')}}" enctype="multipart/form-data">
+                           <button class="btn btn-success">index</button>
+                       </form>
+                    </div>
+                    <div class="col-md-2">
+                       <form action="{{url('PDF/RRCO')}}" enctype="multipart/form-data">
+                           <button class="btn btn-success">pdf</button>
+                       </form>
+                       
+                    </div>
+                    
+                    <div class="col-md-2">
+                       <form action="{{url('export/RRCO')}}" enctype="multipart/form-data">
+                           <button class="btn btn-success">EXCEL</button>
+                       </form>
+                    </div>
+        <!-- <user-component> </user-component> -->
+        <!-- <listado-component> </listado-component> -->
+        <empresas-component> </empresas-component>
         </div>
     </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,39 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\RrcoController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/users', [App\Http\Controllers\UserController::class,'index']);
+Route::get('/paises', [App\Http\Controllers\PaisController::class,'index']);
+Route::get('/empresass', [App\Http\Controllers\EmpresasController::class,'index']);
+Route::get('/paises/export/', [App\Http\Controllers\PaisController::class, 'export']);
+Route::get('/empresass/export/', [App\Http\Controllers\EmpresasController::class,'export']);
+ /// para pdf
+ //Route::get('/imprimir', [App\Http\Controllers\PaisController::class, 'imprimir']);
+
+//Route::get('index', [App\Http\Controllers\ImprimirController::class, 'index']);
+//::get('imprimir', [App\Http\Controllers\ImprimirController::class, 'imprimir']);
+
+
+Route::get('index', [App\Http\Controllers\DdjjsenavexController::class,'index']);
+Route::get('PDF', [App\Http\Controllers\DdjjsenavexController::class,'imprimir']);
+Route::get('export', [App\Http\Controllers\DdjjsenavexController::class,'export']);
+ ///Route::get('PDF', [App\Http\Controllers\ImprimirController::class, 'imprimir']);
+///Route::get('imprimir/{id_empresa}', [App\Http\Controllers\ImprimirController::class, 'show']);
+
+/////rrco
+// Route::get('index/RRCO', [App\Http\Controllers\RrcoController::class,'index']);
+// Route::get('index/RRCO', [App\Http\Controllers\RrcoController::class,'reports_date']);
+// Route::get('report_results', [App\Http\Controllers\RrcoController::class,'report_results']);
+// Route::get('PDF/RRCO', [App\Http\Controllers\RrcoController::class,'imprimir']);
+// Route::get('export/RRCO', [App\Http\Controllers\RrcoController::class,'export']);
+ Route::get('index/RRCO','RrcoController@reports_date')->name('reports.date');
+// Route::get('index/RRCO','RrcoController@index')->name('index');
+Route::get('sales/reports_date','RrcoController@report_results')->name('report.results');
